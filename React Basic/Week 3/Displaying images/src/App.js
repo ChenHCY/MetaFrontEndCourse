36ef32d4008bb89803ae266467d03dd68b94ff99
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-10-09 21:14:39 -0700

    Add files via upload

---
+++ React Basic/Week 3/Displaying images/src/App.js
@@ -0,0 +1,12 @@
+import logo from './assets/logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Task: Add an image below</h1>
+      <img src={logo} height={200} alt="logo"/>
+    </div>
+  );
+}
+sr
+export default App;
