e4afd8360a44268835e410a3d4776b315a460394
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-11-14 01:35:56 -0800

    Add files via upload

---
+++ React Advance/Week 4/src/components/Header.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faLinkedin,
+  faMedium,
+  faStackOverflow,
+} from "@fortawesome/free-brands-svg-icons";
+import { Box, HStack } from "@chakra-ui/react";
+
+//the social links array
+const socials = [
+  {
+    icon: faEnvelope,
+    url: "mailto: hello@example.com",
+  },
+  {
+    icon: faGithub,
+    url: "https://github.com",
+  },
+  {
+    icon: faLinkedin,
+    url: "https://www.linkedin.com",
+  },
+  {
+    icon: faMedium,
+    url: "https://medium.com",
+  },
+  {
+    icon: faStackOverflow,
+    url: "https://stackoverflow.com",
+  },
+];
+
+const Header = () => {
+  const handleClick = (anchor) => () => {
+    const id = `${anchor}-section`;
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
+
+  return (
+    <Box
+      position="fixed"
+      top={0}
+      left={0}
+      right={0}
+      translateY={0}
+      transitionProperty="transform"
+      transitionDuration=".3s"
+      transitionTimingFunction="ease-in-out"
+      backgroundColor="#18181b"
+    >
+      <Box color="white" maxWidth="1280px" margin="0 auto">
+        <HStack
+          px={16}
+          py={4}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <nav>
+            <HStack spacing={6}>
+            {socials.map((social) => (
+              <a href={social.url}> {/* 给图标附上对应的链接*/}
+                {/* FontAwesomeIcon 有两个属性， icon: 要显示的图标， size:图标的大小*/}
+                <FontAwesomeIcon icon={social.icon} size="2x"/> 
+              </a>
+            ))}
+            </HStack>
+          </nav>
+
+          <nav>
+            <HStack spacing={8}>
+              {/* Add internal links to the Projects section and Contact Me section */}
+              <a href="/#projects-section" onClick={()=>handleClick("projects")}>Projects</a>
+              <a href="/#contactme-section" onClick={()=>handleClick("contactme")}>Contact Me</a>
+            </HStack>
+          </nav>
+        </HStack>
+      </Box>
+    </Box>
+  );
+};
+export default Header;
